66e2bde2d08cb3943104c0757085eda2b4e8232b
Oleg Oshmyan <chortos@inbox.lv>
2025-02-27 16:48:44 +0200

    Implement least-squares manually to improve speed & RAM usage
    Vectorize the least-squares computations across samples within the frame,
    and keep only a single frame's worth of data (times a constant factor)
    in memory at any time.
    
    This *could* even be rewritten using VapourSynth's Expr,
    but we're adding many values together,
    so it's nice to have float64 precision (which NumPy provides).

--- hardsubs.py
+++ hardsubs.py
@@ -1,6 +1,7 @@
 import vapoursynth as vs
 import numpy
 from dataclasses import dataclass
+from itertools import accumulate
 from lazy import lazy
 
 
@@ -14,7 +15,6 @@ c = vs.core
 class LazyLeastSquares:
 	op: vs.VideoNode
 	ncop: vs.VideoNode
-	planes_per_subsampling: list[int]
 
 	def __post_init__(self):
 		if not (0 < self.op.width == self.ncop.width and 0 < self.op.height == self.ncop.height and None != self.op.format == self.ncop.format):
@@ -24,66 +24,75 @@ class LazyLeastSquares:
 	def clips(self):
 		op = self.op
 		ncop = self.ncop
-		planes_per_subsampling = self.planes_per_subsampling
 
-		working_dtype = (
-			numpy.float64 if op.format.bits_per_sample > 32
-			              or op.format.sample_type == vs.INTEGER and op.format.bits_per_sample > 24
-			else numpy.float32
-		)
-
-		a = []
-		b = []
-		for iplane in range(op.format.num_planes):
-			if not iplane or iplane == 1 and (op.format.subsampling_h or op.format.subsampling_w):
-				nplanes = planes_per_subsampling[len(a)]
-				height = op.height >> (iplane and op.format.subsampling_h)
-				width = op.width >> (iplane and op.format.subsampling_w)
-				a.append(numpy.zeros((op.num_frames, nplanes, 1 + nplanes, height, width), working_dtype))
-				b.append(numpy.zeros((op.num_frames, nplanes, height, width), working_dtype))
+		plane_subtotal_coef = [0.0] * op.format.num_planes
+		plane_subtotal_sqr_coef = [0.0] * op.format.num_planes
+		plane_subtotal_val = [0.0] * op.format.num_planes
+		plane_dot = [0.0] * op.format.num_planes
 
 		for iframe, (frame, ncframe) in enumerate(zip(op.frames(close=True), ncop.frames(close=True))):
 			for iplane in range(op.format.num_planes):
 				plane = numpy.asarray(frame[iplane])
 				ncplane = numpy.asarray(ncframe[iplane])
-				plane_dtype = plane.dtype
-				if op.format.subsampling_h or op.format.subsampling_w:
-					if not iplane:
-						a[0][iframe, 0, 0] = -ncplane
-						a[0][iframe, 0, 1] = 1
-						b[0][iframe, 0] = plane - ncplane
-					else:
-						a[1][iframe, iplane - 1, 0] = -ncplane
-						a[1][iframe, iplane - 1, iplane] = 1
-						b[1][iframe, iplane - 1] = plane - ncplane
-				else:
-					a[0][iframe, iplane, 0] = -ncplane
-					a[0][iframe, iplane, 1 + iplane] = 1
-					b[0][iframe, iplane] = plane - ncplane
+				input_dtype = plane.dtype
+				plane_subtotal_coef[iplane] -= ncplane
+				plane_subtotal_sqr_coef[iplane] += numpy.square(ncplane, dtype=numpy.float64)
+				val = numpy.subtract(plane, ncplane, dtype=numpy.float64)
+				plane_subtotal_val[iplane] += val
+				plane_dot[iplane] -= numpy.multiply(ncplane, val, dtype=numpy.float64)
+			del plane, ncplane
 
 		alphas = []
 		premultiplieds = []
-		for isubsampling in range(len(a)):
-			nplanes = planes_per_subsampling[isubsampling]
-			height = op.height >> (isubsampling and op.format.subsampling_h)
-			width = op.width >> (isubsampling and op.format.subsampling_w)
-			premultiplieds_planes = [numpy.zeros((height, width), working_dtype) for i in range(nplanes)]
-			alphas_subsampling = numpy.zeros((height, width), working_dtype)
-			for y in range(height):
-				for x in range(width):
-					alpha, *premultiplied_planes = numpy.linalg.lstsq(a[isubsampling][..., y, x].reshape(-1, 1 + nplanes), b[isubsampling][..., y, x].reshape(-1), rcond=None)[0]
-					for i in range(nplanes):
-						premultiplied = premultiplied_planes[i]
-						premultiplieds_planes[i][y, x] = premultiplied
-					alphas_subsampling[y, x] = alpha
+
+		def adapt_sample_type(plane, alpha=False):
 			if op.format.sample_type == vs.INTEGER:
 				peak_value = (1 << op.format.bits_per_sample) - 1
-				alphas_subsampling *= peak_value
-				for plane in premultiplieds_planes:
-					numpy.rint(plane, plane).clip(0, peak_value, plane)
-				numpy.rint(alphas_subsampling, alphas_subsampling).clip(0, peak_value, alphas_subsampling)
-			premultiplieds += [plane.astype(plane_dtype) for plane in premultiplieds_planes]
-			alphas += [alphas_subsampling.astype(plane_dtype)] * nplanes
+				if alpha:
+					plane *= peak_value
+				return (numpy
+					.rint(plane, plane)
+					.clip(0, peak_value, plane)
+					.astype(input_dtype))
+			else:
+				return plane
+
+		def solve(iplane_start, iplane_stop):
+			iplane_slice = slice(iplane_start, iplane_stop)
+			iplane_range = range(iplane_start, iplane_stop)
+
+			total_sqr_coef = sum(plane_subtotal_sqr_coef[iplane_slice])
+			dot = sum(plane_dot[iplane_slice])
+
+			sqr_subtotal_coef = [plane_subtotal_coef[iplane] ** 2 for iplane in iplane_range]
+			subtotal_prod = [plane_subtotal_coef[iplane] * plane_subtotal_val[iplane] for iplane in iplane_range]
+
+			rev_cumsum_sqr_subtotal_coef = list(accumulate(reversed(sqr_subtotal_coef), initial=0))
+			rev_cumsum_subtotal_prod = list(accumulate(reversed(subtotal_prod), initial=0))
+
+			denominator = op.num_frames * total_sqr_coef - rev_cumsum_sqr_subtotal_coef[-1]
+			n_denominator = op.num_frames * denominator
+
+			alphas.extend([adapt_sample_type((op.num_frames * dot - rev_cumsum_subtotal_prod[-1]) / denominator, alpha=True)] * (iplane_stop - iplane_start))
+
+			cumsum_sqr_subtotal_coef = 0
+			cumsum_subtotal_prod = 0
+			for iplane in iplane_range:
+				rest_sqr_subtotal_coef = cumsum_sqr_subtotal_coef + rev_cumsum_sqr_subtotal_coef[iplane_stop - 1 - iplane]
+				rest_subtotal_prod = cumsum_subtotal_prod + rev_cumsum_subtotal_prod[iplane_stop - 1 - iplane]
+				premultiplieds.append(adapt_sample_type(
+					(total_sqr_coef * plane_subtotal_val[iplane] - dot * plane_subtotal_coef[iplane]) / denominator
+					+ (rest_subtotal_prod * plane_subtotal_coef[iplane] - rest_sqr_subtotal_coef * plane_subtotal_val[iplane]) / n_denominator
+				))
+				if iplane < iplane_stop - 1:
+					cumsum_sqr_subtotal_coef += sqr_subtotal_coef[iplane - iplane_start]
+					cumsum_subtotal_prod += subtotal_prod[iplane - iplane_start]
+
+		if op.format.subsampling_h or op.format.subsampling_w:
+			solve(0, 1)
+			solve(1, op.format.num_planes)
+		else:
+			solve(0, op.format.num_planes)
 
 		return premultiplieds, alphas
 
@@ -94,12 +103,7 @@ def extract_hardsubs(op, ncop, first, last, left=0, right=0, top=0, bottom=0):
 	op = op[first:last+1].std.Crop(left=left, right=right, top=top, bottom=bottom)
 	ncop = ncop[first:last+1].std.Crop(left=left, right=right, top=top, bottom=bottom)
 
-	if op.format.subsampling_h or op.format.subsampling_w:
-		planes_per_subsampling = [1, op.format.num_planes - 1]
-	else:
-		planes_per_subsampling = [op.format.num_planes]
-
-	lstsq = LazyLeastSquares(op, ncop, planes_per_subsampling)
+	lstsq = LazyLeastSquares(op, ncop)
 
 	def alphas():
 		return lstsq.clips[1]
